422c559dfc2178481c5849c9ea8e7e7c8298074f
fdelenne <fdelenne@griddynamics.com>
2022-06-20 12:48:44 -0500

    Some errors fixed

--- APIweather.sh
+++ APIweather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+touch output.html
 pathjson="/Users/fdelenne/Documents/ActivityAPI/ans.json"
-
 
 data=$(curl "https://api.openweathermap.org/data/2.5/weather?lat=20.66&lon=-103.33&appid=4b8105e2c121c9cacadf98a85eaa73de&units=metric"
 location="Guadalajara")
@@ -18,16 +18,20 @@ then
     location="Guadalajara")
     echo $data > $pathjson
 fi 
-if [ $(($(date +%s)-$lastUpdateTime)) -ge 1200 ];
+
+if [ $(($(date +%s)-$lastUpdate)) -ge 1200 ];
 then
 lastUpdate=$(date +%s)
-clear 
-#Nombre
-echo "Location:  $(echo $data | jq .name | tr -d '"'),  $(echo $data | jq .sys.country | tr -d '"')"  
-#Temperatura
-echo "Temperature:  $(echo $data | ]jq .main.temp) "Celsius" "
-#Humidity
-echo "Humidity:  $(echo $data | jq .main.humidity) "%" " 
-fi
 
+#Nombre
+echo "Location:  $(echo $data | jq .name | tr -d '"'),  $(echo $data | jq .sys.country | tr -d '"')"  > 'output.html'
+#Temperatura
+echo "Temperature:  $(echo $data | jq .main.temp) "Celsius" " > 'output.html' 
+#Humidity
+echo "Humidity:  $(echo $data | jq .main.humidity) "%" " > 'output.html'
+
+break
+fi
 done
+
+exit 
--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,12 @@ WORKDIR /exe1
 #copy files from the current directory to the previously created (exe1)
 COPY . .
 
+RUN apt-get install curl
+RUN apt-get install jq 
 #Run script
 RUN ./APIweather.sh
-RUN cat output.html
+
+
 #Copy the resultant code from the previous script to "htdocs" dir
 RUN cp output.html /usr/local/apache2/htdocs/index.html
 
