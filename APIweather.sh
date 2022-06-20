6169da54daaf5ef26c08e65417845e31bf4fdd0a
fdelenne <fdelenne@griddynamics.com>
2022-06-20 10:06:49 -0500

    Small Modifications
    Redirecting echos

--- APIweather.sh
+++ APIweather.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-touch output.html
+
 
 data=$(curl "https://api.openweathermap.org/data/2.5/weather?lat=20.66&lon=-103.33&appid=4b8105e2c121c9cacadf98a85eaa73de&units=metric"
 location="Guadalajara")
 
 
-
 #Nombre
-echo "Location:  $(echo $data | jq .name | tr -d '"'),  $(echo $data | jq .sys.country | tr -d '"')" >> output.html
+echo "Location:  $(echo $data | jq .name | tr -d '"'),  $(echo $data | jq .sys.country | tr -d '"')" |  tee 'output.html'
 #Temperatura
-echo "Temperature:  $(echo $data | jq .main.temp) "°C" " >> output.html
+echo "Temperature:  $(echo $data | jq .main.temp) "Celsius" " | tee  'output.html'
 #Humidity
-echo "Humidity:  $(echo $data | jq .main.humidity) "%" " >> output.html
-
- 
-
-
-
+echo "Humidity:  $(echo $data | jq .main.humidity) "%" " | tee 'output.html'
