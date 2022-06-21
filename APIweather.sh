3a619b6fcd6de35f20db3ff3fe5223d4bf31d91f
fdelenne <fdelenne@griddynamics.com>
2022-06-21 13:07:43 -0500

    Fixing errors
    Program runnable with crontab

--- APIweather.sh
+++ APIweather.sh
@@ -13,34 +13,25 @@ echo $data > $pathjson
 
 lastUpdate=$(($(date +%s) -1200))
 
-while true 
-do
-    lastfileupdate=$(date -r $pathjson +%s)
-if [ $(($(date +%s)-$lastfileupdate)) -ge 1200 ];
-then
-    data=$(curl "https://api.openweathermap.org/data/2.5/weather?lat=20.66&lon=-103.33&appid=4b8105e2c121c9cacadf98a85eaa73de&units=metric"
-    location="Guadalajara")
-    echo $data > $pathjson
-fi 
+lastfileupdate=$(date -r $pathjson +%s)
+    if [ $(($(date +%s)-$lastfileupdate)) -ge 1200 ];
+    then
+            data=$(curl "https://api.openweathermap.org/data/2.5/weather?lat=20.66&lon=-103.33&appid=4b8105e2c121c9cacadf98a85eaa73de&units=metric"
+            location="Guadalajara")
+            echo $data > $pathjson
+    fi 
 
-if [ $(($(date +%s)-$lastUpdate)) -ge 1200 ];
-then
-lastUpdate=$(date +%s)
+    if [ $(($(date +%s)-$lastUpdate)) -ge 1200 ];
+    then
+        lastUpdate=$(date +%s)
 
-#Nombre
-echo -e "Location:  $(echo $data | jq .name | tr -d '"'),  $(echo $data | jq .sys.country | tr -d '"')"  > 'output.html'
-#Temperatura
-echo  -e " Temperature:  $(echo $data | jq .main.temp) "Celsius" " > 'output.html'
-#Humidity
-echo -e " Humidity:  $(echo $data | jq .main.humidity) "%" "  > 'output.html'
+        #Nombre
+        echo "Location:  $(echo $data | jq .name | tr -d '"'),  $(echo $data | jq .sys.country | tr -d '"')"  > 'output.html'
+        #Temperatura
+        echo " Temperature:  $(echo $data | jq .main.temp) "Celsius" " >> 'output.html'
+        #Humidity
+        echo " Humidity:  $(echo $data | jq .main.humidity) "%" "  >> 'output.html'
 
-fi
+    fi
 
-if [ $dynamic -eq 0 ];
-then
-break
-fi
 
-done
-
-exit 
